083ed387da4f2e149dde5e274751fbc7cd50f4d3
kimjongwoo-cell <59594139+kimjongwoo-cell@users.noreply.github.com>
2025-08-05 12:34:40 +0000

    Deploying to gh-pages from @ kimjongwoo-cell/kimjongwoo-cell.github.io@6dc439cec08469ba120bc7a2ca8167d6a71e794a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,18 +10,18 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "All the publications that I have done or collaborated with. (†:Equal contribution)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/cvcv.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
